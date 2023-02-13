06ed2cd9484b1435d490d42eba35dd4ea58188e3
vishal sangole <wishalsangole4444@gmail.com>
2023-02-13 12:08:01 +0530

    used redux persist for persistance storage and rehydration

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { createStore , applyMiddleware, compose} from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root.reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const loggerMiddleware = (store) => (next) => (action) =>{
     if(!action.type) return next(action);
@@ -14,8 +16,17 @@ const loggerMiddleware = (store) => (next) => (action) =>{
     console.log("NEXT_STATE : ", store.getState());
 }
 
+const persistconfig = {
+    key: "root",
+    storage,
+    blacklist: ["user"]
+}
+
+const persistedReducer = persistReducer(persistconfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 const composedEnhancer = compose(applyMiddleware(... middleWares));
 
-export const store = createStore(rootReducer, undefined,composedEnhancer);
+export const store = createStore(persistedReducer, undefined, composedEnhancer);
 
+export const persistor = persistStore(store);
